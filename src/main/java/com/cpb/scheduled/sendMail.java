60ff40402e95bce06506ef63e2b715d1e0e3fc88
YemaoLuo <leafysn@qq.com>
2021-11-13 16:51:18 +0800

    Add Scheduled tasks

--- src/main/java/com/cpb/scheduled/sendMail.java
+++ src/main/java/com/cpb/scheduled/sendMail.java
@@ -25,12 +25,19 @@ public class sendMail {
     private submitMapper submitMapper;
 
     @Scheduled(cron = "0 */1 * * * ?")
-    public void sendMail(){
+    public void sendMail() {
         System.out.println("Mail send...");
         List<user> allUsers = submitMapper.findAllUsers();
-        for (user user: allUsers){
-            String email = submitMapper.finduserByName(crushMapper.findSingleCrush(user.getName()).getUname()).getEmail();
-            MailUtils.sendMail(email, "两情相悦", "USSTCrush的祝福");
+        for (user user : allUsers) {
+            user user1 = submitMapper.finduserByName(crushMapper.findSingleCrush(user.getName()).getUname());
+            crush crush = crushMapper.findCrush(user.getName(), user1.getName());
+            if (user1 != null && crush.getChecked() == false) {
+                String email = user1.getEmail();
+                MailUtils.sendMail(email, "两情相悦", "USSTCrush的祝福");
+                MailUtils.sendMail(user.getEmail(), "两情相悦", "USSTCrush的祝福");
+                crushMapper.updateCrushChecked(user1.getName());
+                crushMapper.updateCrushChecked(user.getName());
+            }
         }
     }
 }
